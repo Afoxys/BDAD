ea414cb4f9e5a190d349a6b2288fd515c13b4541
Afoxys <38985979+Afoxys@users.noreply.github.com>
2019-04-14 17:01:06 +0100

    Added missing ";"

--- criar.sql
+++ criar.sql
@@ -53,64 +53,64 @@ CREATE TABLE Dia (
   date DATETIME,
   startHour INTEGER,
   endHour INTEGER
-)
+);
 CREATE TABLE Banda (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
   contacto TEXT
-)
+);
 
 CREATE TABLE Membro (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT
-)
+);
 
 CREATE TABLE Genero (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT
-)
+);
 
 /* many-to-many genero banda */
 CREATE TABLE GeneroBanda (
   bandaId INTEGER NOT NULL,
   generoId INTEGER NOT NULL,
   PRIMARY KEY (bandaId, generoId)
-)
+);
 
 /* many-to-many membro banda */
 CREATE TABLE MembroBanda (
   bandaId INTEGER NOT NULL,
   membroId INTEGER NOT NULL,
   PRIMARY KEY (bandaId, membroId)
-)
+);
 
 
 
 CREATE TABLE Infrastrutura (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   disponibilidade INTEGER
-)
+);
 /* < Sub classes de infrastrutura > */
 CREATE TABLE Palco (
   infrastruturaId INTEGER PRIMARY KEY REFERENCES Infrastrutura(id),
   name TEXT
-)
+);
 CREATE TABLE WC (
   infrastruturaId INTEGER PRIMARY KEY REFERENCES Infrastrutura(id),
   showers INTEGER,
   toilets INTEGER,
   sinks INTEGER
-)
+);
 CREATE TABLE Restauracao (
   infrastruturaId INTEGER PRIMARY KEY REFERENCES Infrastrutura(id),
   tables INTEGER,
   chairs INTEGER
-)
+);
 CREATE TABLE Campismo (
   infrastruturaId INTEGER PRIMARY KEY REFERENCES Infrastrutura(id),
   campingZones INTEGER,
   trashCans INTEGER
-)
+);
 /* </ Sub classes de infrastrutura > */
 
 /* many-to-many genero palco */
@@ -118,23 +118,23 @@ CREATE TABLE GeneroPalco (
   palcoId INTEGER NOT NULL REFERENCES Palco(id),
   generoId INTEGER NOT NULL REFERENCES Genero(id),
   PRIMARY KEY (palcoId, generoId)
-)
+);
 
 CREATE TABLE Staff (
   id INTEGER PRIMARY KEY AUTOINCREMENT
-)
+);
 CREATE TABLE SubContrato (
   staffId INTEGER PRIMARY KEY REFERENCES Staff(id),
   empresa TEXT
-)
+);
 CREATE TABLE Sponsor (
   staffId INTEGER PRIMARY KEY REFERENCES Staff(id),
   empresa TEXT
-)
+);
 
 /* many-to-many staff infrastrutura  */
 CREATE TABLE StaffInfrastrutura (
   staffId INTEGER NOT NULL REFERENCES Staff(id),
   infrastruturaId INTEGER NOT NULL REFERENCES Infrastrutura(id),
   PRIMARY KEY (staffId, infrastruturaId)
-)
+);
