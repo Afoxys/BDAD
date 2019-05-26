5b2f1b9873089d923a2b47b79d5297b90dc12e60
Afoxys <38985979+Afoxys@users.noreply.github.com>
2019-05-26 22:14:14 +0100

    Update run.sh

--- 3rdSub/run.sh
+++ 3rdSub/run.sh
@@ -3,3 +3,13 @@
 .read popular.sql
 
 .read int1.sql
+.read int2.sql
+.read int3.sql
+.read int4.sql
+.read int5.sql
+.read int6.sql
+.read int7.sql
+.read int8.sql
+.read int9.sql
+.read int10.sql
+
