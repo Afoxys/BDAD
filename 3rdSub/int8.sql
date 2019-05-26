97e48af06dfcbbae12ccec8bdac50057c66fc650
Afoxys <38985979+Afoxys@users.noreply.github.com>
2019-05-26 22:53:20 +0100

    Update int8.sql

--- 3rdSub/int8.sql
+++ 3rdSub/int8.sql
@@ -6,9 +6,3 @@ SELECT Palco.name AS Palco, Genero.name AS Genero
 FROM Palco
 INNER JOIN GeneroPalco, Genero
 WHERE GeneroPalco.palcoId = Palco.id and Genero.id = GeneroPalco.generoId;
-
-/*
-INNER JOIN GeneroBanda 
-ON GeneroBanda.bandaId = Banda.id
-INNER JOIN Genero 
-ON Genero.id = GeneroBanda.GeneroId;*/
